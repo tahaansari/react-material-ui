6639b195568c211e01bd5998e4278381ebd15b8d
Taha Ansari <taha.m.ansari@gmail.com>
2024-03-17 15:22:37 +0300

    WIP

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { Typography, Button } from "@mui/material";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <div>Hello World!!</div>
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
-      <Typography variant="h1">I am heading</Typography>
+      <Typography variant="h1" gutterBottom sx={{backgroundColor:"red", color:"white"}}>I am heading</Typography>
+      <NavBar></NavBar>
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import { AppBar } from '@mui/material'
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div>
+        <AppBar position="fixed">
+
+        </AppBar>
+    </div>
+  )
+}
+
+export default NavBar
